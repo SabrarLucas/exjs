7ef9c82ade7f0efca6c09568b5411e88a36624c1
SabrarLucas <pinchonlucas03@gmail.com>
2022-10-27 12:58:13 +0200

    eval

--- petit_bonus/js/javascript.js
+++ petit_bonus/js/javascript.js
@@ -3,170 +3,117 @@ var bt2 = document.getElementById("bt2");
 var bt3 = document.getElementById("bt3");
 var bt4 = document.getElementById("bt4");
 
+/*-------------------------------- EX 1 -----------------------------*/
 function Ex1(){
-    var nom = window.prompt("Saisissez votre nom");
-    var prenom = window.prompt("Saisissez votre prénom");
-    var sexe = window.prompt("Quel est votre sexe ? (H/F)")
-    var ident;
-    if (sexe == "H"){
-        if (window.confirm("Vous êtes un homme")){
-            ident = "Monsieur";
-        }
-    }
-    else if (sexe == "F"){
-        if (window.confirm("Vous êtes une femme")){
-            ident = "Madame";
-        }
-    }
-    else{
-        window.confirm("Il y a une erreur");
-    }
-
-    window.alert("Bonjour " + ident + " " + nom + " " + prenom);
-}
-
-function Ex2(){
-    var a = "100";
-    var b = 100;
-    var c = 1.00;
-    var d = new Boolean(1);
-
-    window.alert("Ceci est une chaîne de caractère : " + a);
-    b--;
-    window.alert(b);
-    window.confirm(parseInt(a)+c);
-    window.confirm(!d);
-}
-
-function Ex3(){
-    alert("Nombre pair ou impaire");
-    var val = window.prompt("Entrée un nombre");
-    var res;
-    res = parseInt(val)%2;
-    if(res==0){
-        window.alert(val + " est un nombre pair");
-    }
-    else{
-        window.alert(val + " est un nombre impair");
-    }
-
-    ///////////////////////////////////////////////////////
-    alert("Exo sur l'âge");
-    var annee = 2022;
     var age;
-    var date = prompt("Donnée votre année de naissance");
+    var jeune = 0;
+    var moyen = 0;
+    var vieux = 0;
+    var arret = 0;
+    var i = 0;
 
-    age = annee-date;
+    while(arret != -1){
+        age = parseInt(prompt("Donnez l'âge"));
+        if(age >= 100){
+            arret = -1;
+        }else if(age < 20){
+            jeune = jeune + 1;
+        }else if(age > 40){
+            vieux = vieux + 1;
+        }else if((age >= 20) && (age <= 40)){
+            moyen = moyen + 1;
+        }
 
-    (age >= 18) ? console.log("Vous êtes majeur") : console.log("Vous êtes mineur");
-    alert("Vous avez : " + age);
+        i++;
+    }
+    console.log("prenom saisie : "+i);
+    alert("Il y a "+jeune+" personnes de moins de 20 ans.\nIl y a "+moyen+" personnes entre 20 et 40 ans.\nIl y a "+vieux+" personnes de plus de 40 ans.")
+}
 
-    ///////////////////////////////////////////////////////
-    alert("Calculatrice");
-    var val1 = prompt("Saisir la 1er valeur");
-    var ope = prompt("Saisir un opérateur(+, -, *, /)");
-    var val2 = prompt("Saisir la 2eme valeur");
+/*-------------------------------- EX 2 -----------------------------*/
+function Ex2(){
+    var table = parseInt(prompt("Quelle table calculer ?"));
 
-    switch (ope)
-    {
-        case "+":
-            res = parseInt(val1) + parseInt(val2);
-            break;
-        
-        case "-":
-            res = parseInt(val1) - parseInt(val2);
-            break;
+    function TableMultiplication(x){
+        var i = 0;
+        var res = 0;
+        for(i; i <= 10; i++){
+            res = i * x;
+            console.log(res);
+        }
+    }
+    TableMultiplication(table);
+}
 
-        case "*":
-            res = parseInt(val1) * parseInt(val2);
-            break;
+/*-------------------------------- EX 3 -----------------------------*/
+function Ex3(){
+    const tab = ["audrey","aurelien", "flavien", "jeremy", "laurent", "melik", "nouara", "salem", "samuel", "stephane"];
+    const saisie = window.prompt(`Veuillez choisir un prénom parmi ${[...tab]}.`)
+    if(tab.includes(saisie)){
+	    tab.splice(tab.indexOf(saisie),1);
+	    tab.push(" ");
+	    console.log(tab)
+    }
+    else{
+        alert("erreur")
+    }
+}
 
-        case "/":
-            if(val2==0){
-                alert("Division par 0 impossible")
+/*-------------------------------- EX 4 -----------------------------*/
+function Ex4(){
+    var PU = parseInt(prompt("Saisir le prix unitaire."));
+    var QTECOM = parseInt(prompt("Saisir la quantité commandée."));
+    var PAP;
+    var REM;
+    var PORT;
+    var res;
+    var TOT = PU * QTECOM;
+
+    if(TOT >= 100 && TOT <= 200){
+        REM = TOT * 5/100;
+        PAP = TOT - REM;
+        if(PAP >= 500){
+            PORT = 0;
+            alert("remise de 5% (-"+REM+") soit "+PAP+"€ et frais de port "+PORT+" à payer : "+PAP+"€");
+        }
+        else{
+            PORT = PAP * 2/100;
+            if(PORT < 6){
+                res = PAP + 6;
+                alert("remise de 5% (-"+REM+") soit "+PAP+"€ et frais de port 2% soit "+PORT.toFixed(2)+"€ mini 6€; à payer : "+res.toFixed(2)+"€");
             }
             else{
-                res = parseInt(val1) / parseInt(val2);
+                res = PAP + PORT;
+                alert("remise de 5% (-"+REM+") soit "+PAP+"€ et frais de port 2% soit "+PORT.toFixed(2)+"€; à payer : "+res.toFixed(2)+"€");
             }
-            break;
-
-        default:
-            alert("Erreur sur l'opérateur");
-    }
-
-    alert("Le resultat est : " + res);
-}
-
-function Ex4(){
-    alert("Exo 1");
-    var i = 1;
-    var prenom
-    alert("envoyer rien pour arreter la saisie");
-
-    while(prenom!=""){
-        prenom = prompt("Saisissez le prénom N°" + i);
-        console.log("Prénom N°"+i+" : "+prenom);
-        i++;
-    }
-    i = i-2;
-    console.log("==================");
-    console.log("Vous avez saisie "+i+" prénom");
-
-    ///////////////////////////////////////////////////////
-    alert("Exo 2");
-    var val = prompt("Saisissez un nombre");
-    var i = 0;
-
-    for(i;i<=val;i++){
-        console.log(i);
-    }
-
-    ///////////////////////////////////////////////////////
-    alert("Exo 3");
-    var val;
-    var somme = 0;
-    var moyenne = 0;
-    var i = 0;
-
-    do{
-        val = parseInt(prompt("Saisir une valeur"));
-        somme = somme+val;
-        i++;
-    }while(val!=0)
-    i = i-1;
-    console.log("La somme est : "+somme);
-    moyenne = somme/i;
-    console.log("La moyenne est : "+moyenne);
-
-    ///////////////////////////////////////////////////////
-    alert("Exo 4");
-    var N = parseInt(prompt("Saisire le nombres de multiples"));
-    var X = parseInt(prompt("Saisire la valeur à calculer"));
-    var i = 1;
-    var res = 0;
-
-    for(i; i<=N;i++){
-        res=i*X;
-        console.log(i + " x " + X + " = " + res);
-    }
-
-    ///////////////////////////////////////////////////////
-    alert("Exo 5");
-    var mot = prompt("Saisir une mot");
-    var nbLettre = mot.length;
-    var i=0;
-    var lettre;
-    var nbVoyelle = 0;
-
-    while(i<=nbLettre){
-        lettre = mot.substring(i, i+1);
-        console.log(lettre); 
-        i++;
-        if(lettre=="a" || lettre=="e" || lettre=="i" || lettre=="o" || lettre=="u" || lettre=="y"){
-            nbVoyelle = nbVoyelle+1;
         }
     }
-    console.log("Il y a " + nbVoyelle + " voyelle dans le mot " + mot);
+    else if(TOT > 200){
+        REM = TOT * 10/100;
+        PAP = TOT - REM;
+        if(PAP >= 500){
+            PORT = 0;
+            alert("remise de 10% (-"+REM+") soit "+PAP+"€ et frais de port = "+PORT+"; à payer : "+PAP.toFixed(2)+"€");
+        }
+        else{
+            PORT = PAP * 2/100;
+            res = PAP + PORT;
+            alert("remise de 10% (-"+REM+") soit "+PAP+"€ et frais de port 2% soit +"+PORT.toFixed(2)+"€; à payer : "+res.toFixed(2)+"€");
+        }
+    }
+    else if(TOT < 100){
+        REM = 0;
+        PAP = TOT - REM;
+        PORT = PAP * 2/100;
+        if(PORT < 6){
+            res = PAP + 6;
+            alert("remise de "+REM+" et frais de port 2% soit "+PORT.toFixed(2)+"€ mini 6€; à payer : "+res.toFixed(2)+"€");
+        }
+        else{
+            res = PAP + PORT;
+            alert("remise de "+REM+" et frais de port 2% soit "+PORT.toFixed(2)+"€; à payer : "+res.toFixed(2)+"€");
+        }
+    }
 }
 
 
